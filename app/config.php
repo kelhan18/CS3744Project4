50b9b059758d3de216df902139f5cf88559b81c7
tonym301 <30272917+tonym301@users.noreply.github.com>
2018-03-30 16:10:03 -0400

    db connect abaility

--- app/config.php
+++ app/config.php
@@ -6,8 +6,8 @@ define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't cha
 define('BASE_URL','http://ec2-18-222-16-185.us-east-2.compute.amazonaws.com/CS3744Project4'); # base URL for server
 
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'CS3744!P4');
 define('DB_HOST', 'localhost');
-define('DB_DATABASE', 'stark');
+define('DB_DATABASE', 'peleliu');
 
 ?>
