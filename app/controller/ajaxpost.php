d79e2f561fd0465bbb96fb46d614ebb0c6af1a01
Campbell Johnson <campbel1@vt.edu>
2018-03-31 19:05:16 -0400

    ajax

--- app/controller/ajaxpost.php
+++ app/controller/ajaxpost.php
@@ -1,5 +1,5 @@
 <?php
   $description = $_POST['description']; // required
-  // $profile_id = $_POST['profile_id']; // required
-  // $topic_id = $_POST['topic_id']; // required
+  $profile_id = $_POST['profile_id']; // required
+  $topic_id = $_POST['topic_id']; // required
   // echo $description';
