a654bd2f91ae828bf8c2efc8cd249c9a3aa9e80c
Campbell Johnson <campbel1@vt.edu>
2018-03-31 19:21:03 -0400

    ajax

--- app/controller/topicController.php
+++ app/controller/topicController.php
@@ -48,7 +48,15 @@ class TopicController
         $description = $_POST['description']; // required
         $profile_id = $_POST['profile_id']; // required
         $topic_id = $_POST['topic_id']; // required
-        echo "<script type='text/javascript'>alert('working!');</script>";
+
+        $mypost = new Post();
+        $mypost->description  = $description;
+        $mypost->date_posted  = date("Y-m-d");
+        $mypost->number_posts = 0;
+        $mypost->topic_id = $topic_id;
+        $mypost->profile_id = $profile_id;
+
+        $post_id = $mypost->save($profile_id, $topic_id);
     }
 
     public function newPost($topic_id)
