f07fe3c3fa5157eb2bcee350b47c0c839b8e03a2
Campbell Johnson <campbel1@vt.edu>
2018-03-31 19:18:36 -0400

    ajax

--- app/controller/ajaxpost.php
+++ app/controller/ajaxpost.php
@@ -2,4 +2,12 @@
   $description = $_POST['description']; // required
   $profile_id = $_POST['profile_id']; // required
   $topic_id = $_POST['topic_id']; // required
-  echo $description;
+
+  $mypost = new Post();
+  $mypost->description  = $description;
+  $mypost->date_posted  = date("Y-m-d");
+  $mypost->number_posts = 0;
+  $mypost->topic_id = $topic_id;
+  $mypost->profile_id = $profile_id;
+
+  $post_id = $mypost->save($profile_id, $topic_id);
