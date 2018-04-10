8042e4b9226197301986b1fdfbe2701d2bd9cb18
Keller Han <keller@Kellers-MacBook-Pro.local>
2018-04-10 19:06:56 -0400

    Working on backend to account for new user fields: email, address, timezone

--- app/controller/siteController.php
+++ app/controller/siteController.php
@@ -174,11 +174,14 @@ class SiteController
 
         $profile_id = $profile->save(0);
 
+        /*
         if ($profile_id == null)
         {
             header('Location: '.BASE_URL); exit();
         }
         header('Location: '.BASE_URL.'/myAccount/'); exit();
+        */
+
     }
 
 
