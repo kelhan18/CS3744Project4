ab60d3ec1032c5485d8863955e39baebeef17732
Campbell Johnson <campbel1@vt.edu>
2018-04-12 12:19:50 -0400

    debug

--- app/model/Activity.class.php
+++ app/model/Activity.class.php
@@ -8,36 +8,36 @@ class Activity {
   public $profile_id = 0;
   public $description = '';
 
-  // return all activities for a given profile id in an array
-  public function getActivities($profile_id) {
-    $db = Db::instance();
-    $q = "SELECT * FROM `".self::DB_TABLE."` WHERE `profile_id` = ".$profile_id;
-    $result = $db->query($q);
-
-    $activities = array();
-    if($result->num_rows != 0) {
-      while($row = $result->fetch_assoc()) {
-        $activities[] = $row['description'];
-      }
-    }
-    return $activities;
-  }
-
-  // adds a new activity
-  public function addActivity($profile_id, $description) {
-    $db = Db::instance();
-    $q = sprintf("INSERT INTO `activity` (`profile_id`, `description`)
-    VALUES (%d, %s);",
-      $profile_id,
-      $db->escape($description)
-    );
-
-    $result = $db->query($q); // execute query
-    return $db->getInsertID();
-  }
+  // // return all activities for a given profile id in an array
+  // public function getActivities($profile_id) {
+  //   $db = Db::instance();
+  //   $q = "SELECT * FROM `".self::DB_TABLE."` WHERE `profile_id` = ".$profile_id;
+  //   $result = $db->query($q);
+  //
+  //   $activities = array();
+  //   if($result->num_rows != 0) {
+  //     while($row = $result->fetch_assoc()) {
+  //       $activities[] = $row['description'];
+  //     }
+  //   }
+  //   return $activities;
+  // }
+  //
+  // // adds a new activity
+  // public function addActivity($profile_id, $description) {
+  //   $db = Db::instance();
+  //   $q = sprintf("INSERT INTO `activity` (`profile_id`, `description`)
+  //   VALUES (%d, %s);",
+  //     $profile_id,
+  //     $db->escape($description)
+  //   );
+  //
+  //   $result = $db->query($q); // execute query
+  //   return $db->getInsertID();
+  // }
 
   // returns returnZero
   public function returnZero() {
-    return echo("<script>console.log('PHP: In ACtivity');</script>");
+    return "<script>console.log('PHP: In ACtivity');</script>";
   }
 }
